9881c51480dcbe2d3b15a0f23a3906f3e1a72b99
jgverges <55912813+jgverges@users.noreply.github.com>
2020-11-14 18:15:25 +0100

    Assigment 3  with reactstrap

--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import { Button,Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem, Nav,  NavItem, 
-     Modal, ModalHeader, ModalBody,Form, FormGroup, Input, Label } from 'reactstrap';
+import { Button,Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem, Form, FormGroup, FormFeedback, Nav,  NavItem, 
+     Modal, ModalHeader, ModalBody, Input, Label } from 'reactstrap';
 import { Link } from 'react-router-dom';
 
 
@@ -84,20 +84,34 @@ else
 export default  DishDetail;
 
 
+/* ***************************************************** */
 /**********  CommentForm ******************************* */
+/* ***************************************************** */
 
 class CommentForm extends React.Component{
     constructor(props) {
         super(props);
     
-        this.toggleNav = this.toggleNav.bind(this);
-        this.toggleModal = this.toggleModal.bind(this);
-        this.handleComments = this.handleComments.bind(this);
-        
+                
         this.state = {
             isNavOpen: false,
-            isModalOpen: false
-        };
+            isModalOpen: false,
+            rating:'',
+            name : '',
+            comment:'',
+            touched: {
+                rating:false,
+                name : false,
+                comment:false
+            }
+        }
+
+        this.toggleNav = this.toggleNav.bind(this);
+        this.toggleModal = this.toggleModal.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleInputChange = this.handleInputChange.bind(this);
+        this.handleBlur = this.handleBlur.bind(this);
+
       }
 
       toggleNav() {
@@ -112,8 +126,43 @@ class CommentForm extends React.Component{
         });
       }
       
-      handleComments(event) {
+      handleBlur = (field) => (evt) => {
+        this.setState({
+            touched: { ...this.state.touched, [field]: true }
+        });
+    }
+
+    validate( name, comment) {
+        const errors = {
+            name: '',
+            comment:''
+        };
+        if (this.state.touched.name && name.length < 3)
+            errors.name = 'Name should be >= 3 characters';
+        else if (this.state.touched.name && name.length >= 15)
+            errors.name = 'Name should be <= 15 characters';
+
+        if (this.state.touched.comment && comment.length <3) 
+            errors.comment = 'Comment shoul be >=3 characters';
+
+        return errors;
+    }
+
+    handleInputChange(event) {
+        const target = event.target;
+        const value = target.type === 'select' ? target.selected : target.value;
+        const name = target.name;
+    
+        this.setState({
+          [name]: value
+        });
+    }
+
+      handleSubmit(event) {
         this.toggleModal();
+        console.log("Rating: " + this.rating.value + 
+                    " Name: " + this.name.value +
+                    "Comment: "+ this.comment.value);
         alert("Rating: " + this.rating.value + 
               " Name: " + this.name.value +
               "Comment: "+ this.comment.value
@@ -121,7 +170,10 @@ class CommentForm extends React.Component{
         event.preventDefault();
 
     }
+
+
     render(){
+        const errors = this.validate(this.state.name, this.state.comment);
         return(
           <React.Fragment>
 
@@ -137,7 +189,7 @@ class CommentForm extends React.Component{
             <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
                 <ModalHeader toggle={this.toggleModal}>Submit Comment</ModalHeader>
                 <ModalBody>
-                <Form onSubmit={this.handleComments}>
+                <Form onSubmit={this.handleSubmit}>
                         <FormGroup>
                             <Label htmlFor="rating">Rating</Label>                            
                             <Input type="select" id="rating" name="rating"
@@ -151,14 +203,26 @@ class CommentForm extends React.Component{
                         </FormGroup>
                         <FormGroup>
                             <Label htmlFor="name">Your Name</Label>
-                            <Input type="name" id="name" name="name"
-                                innerRef={(input) => this.name = input}  />
+                            <Input type="text" id="name" name="name"                                       
+                                        value={this.state.name}
+                                        valid={errors.name === ''}
+                                        invalid={errors.name !== ''}
+                                        onBlur={this.handleBlur('name')}
+                                        onChange={this.handleInputChange}
+                                        innerRef={(input) => this.name = input}  />
+                                    <FormFeedback>{errors.name}</FormFeedback>                              
                         </FormGroup>
                         <FormGroup>
                             <Label htmlFor="comment">Comment</Label>
                             <Input type="textarea" id="comment" name="comment"
-                                innerRef={(input) => this.comment = input} 
+                                innerRef={(input) => this.comment = input}
+                                value={this.state.comment}
+                                valid={errors.comment === ''}
+                                invalid={errors.comment !== ''}
+                                onBlur={this.handleBlur('comment')}
+                                onChange={this.handleInputChange} 
                                 rows="6" />
+                                 <FormFeedback>{errors.comment}</FormFeedback>    
                         </FormGroup>
                        
                         <Button type="submit" value="submit" color="primary">Submit</Button>
